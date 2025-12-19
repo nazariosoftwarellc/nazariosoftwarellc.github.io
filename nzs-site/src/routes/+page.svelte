23ecbef4580d4fd88f2393bc8f54d7dca0dbfc4c
Kyle Nazario <kylebnazario@gmail.com>
2025-12-19 13:49:34 -0700

    Top of spash page

--- nzs-site/src/routes/+page.svelte
+++ nzs-site/src/routes/+page.svelte
@@ -1,4 +1,23 @@
-<header>
-  <h1>Nazario Software</h1>
-  <h2>Hand-raised, free-range browser extensions</h2>
+<script lang="ts">
+	import siteSplashImg from '$lib/assets/img/site-splash.png';
+</script>
+
+<header style:--bg-image="url({siteSplashImg})">
+	<div>
+		<h1>Nazario Software</h1>
+		<h2>Hand-raised, free-range browser extensions</h2>
+	</div>
 </header>
+
+<style lang="scss">
+	header {
+		height: 700px;
+		background-image: var(--bg-image);
+		background-position: center;
+
+		h1,
+		h2 {
+			text-shadow: 0 2px 4px rgba(0, 0, 0, 0.3);
+		}
+	}
+</style>
