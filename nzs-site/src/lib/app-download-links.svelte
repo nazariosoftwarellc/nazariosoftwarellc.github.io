07aded64dd447f7588492ca460b5b4ac5f0d09ad
Kyle Nazario <kylebnazario@gmail.com>
2025-12-21 22:29:39 -0700

    Chrome download link

--- nzs-site/src/lib/app-download-links.svelte
+++ nzs-site/src/lib/app-download-links.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import type { ResolvedPost } from '$lib/types/resolved-types';
 	import AppStoreLink from './app-store-link.svelte';
+	import ChromeStoreLink from './chrome-store-link.svelte';
 
   const { downloadLinks }: { downloadLinks: ResolvedPost['downloadLinks'] } = $props();
 </script>
@@ -9,11 +10,15 @@
   {#if downloadLinks.appleStoreUrl}
     <AppStoreLink href={downloadLinks.appleStoreUrl} />
   {/if}
+  {#if downloadLinks.chromeStoreUrl}
+    <ChromeStoreLink href={downloadLinks.chromeStoreUrl} />
+  {/if}
 </div>
 
 <style>
   .download-links {
     display: flex;
     justify-content: center;
+    gap: 1rem;
   }
 </style>
--- nzs-site/src/lib/app-store-link.svelte
+++ nzs-site/src/lib/app-store-link.svelte
@@ -1,11 +1,19 @@
 <script lang="ts">
-  import appStoreBadge from '$lib/assets/img/app-store-badge.svg';
-  const { href }: { href: string } = $props();
+	import appStoreBadge from '$lib/assets/img/app-store-badge.svg';
+	const { href }: { href: string } = $props();
 </script>
 
-<div style="height: 6rem;width: auto;">
-    <a {href} target="_blank" title="Download on the App Store">
-        <img src={appStoreBadge} alt="Download on the App Store"
-             style="height: 100%; width: auto;">
-    </a>
+<div>
+	<a {href} target="_blank" title="Download on the App Store">
+		<img src={appStoreBadge} alt="Download on the App Store" style="height: 100%; width: auto;" />
+	</a>
 </div>
+
+<style lang="scss">
+  @use '../variables';
+
+  div {
+    height: variables.$app-store-badge-height;
+    width: auto;
+  }
+</style>
