8ff7320ceb2fc863ec567eaaa3e7abb31bd7bdc4
Kyle Nazario <kylebnazario@gmail.com>
2026-06-28 16:37:26 -0600

    Two cols for desktop app list

--- src/lib/app-link-list.svelte
+++ src/lib/app-link-list.svelte
@@ -5,7 +5,7 @@
 	const { appList }: { appList: NZSAppList } = $props();
 </script>
 
-<div class="outlined-section">
+<div class="outlined-section list-container">
 	{#each appList as app (app.id)}
 		<div class="app-link-item">
 			<AppLink {...app} />
@@ -15,6 +15,15 @@
 
 <style lang="scss">
 	@use '../global-styles';
+	@use '../variables';
+
+	@media screen and (min-width: variables.$mobile-width) {
+		.list-container {
+			display: grid;
+			grid-template-columns: 1fr 1fr;
+			gap: 1.5em;
+		}
+	}
 
 	.app-link-item {
 		margin-bottom: 1.5em;
